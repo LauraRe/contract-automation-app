ad1e0fa7e6c054712293be5b5af0049d006c0a7d
Laura Reale <laaru1988@gmail.com>
2020-09-25 16:46:09 +0200

    delete customer's contracts when customer is deleted, pass contracts to Customer

--- src/App.js
+++ src/App.js
@@ -6,18 +6,25 @@ import { contracts } from './contracts';
 
 const App = () => {
   const [customersList, setCustomersList] = useState(customers);
+  const [contractsList, setContractsList] = useState(contracts);
  
   const handleDeleteCustomer = (id) => {
     const newCustomersList = customersList.filter((customer) => customer.id !== id);
     setCustomersList(newCustomersList);
+    handleContracts(id)
+  }
+
+  const handleContracts = (customerId) => {
+    const newContractsList = contractsList.filter((contract) => contract.customerId !== customerId);
+    setContractsList(newContractsList);
   }
 
   return (
     <>
       <h1>Customers:</h1>
-      <CustomersList customers={customersList} onDelete={handleDeleteCustomer} />
+      <CustomersList contracts={contractsList} customers={customersList} onDelete={handleDeleteCustomer} />
       <h1>Contracts:</h1>
-      <ContractsList contracts={contracts} />
+      <ContractsList contracts={contractsList} />
     </>
   )
 }
