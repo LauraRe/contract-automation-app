44d53de57da2e7fc0468ccce6d8c50de2aff8622
Laura Reale <laaru1988@gmail.com>
2020-09-27 13:00:17 +0200

    refactor rootReducer

--- src/state/reducers/rootReducer.js
+++ src/state/reducers/rootReducer.js
@@ -2,12 +2,10 @@ import initialState from '../store/initialState';
 
 const rootReducer = (state = initialState, action) => {
   if (action.type === 'DELETE_CUSTOMER') {
-    const newCustomersList = state.customers.filter((customer) => customer.id !== action.id);
-    const newContractsList = state.contracts.filter((contract) => contract.customerId !== action.id);
     return {
       ...state,
-      customers: newCustomersList,
-      contracts: newContractsList
+      customers: state.customers.filter((customer) => customer.id !== action.id),
+      contracts: state.contracts.filter((contract) => contract.customerId !== action.id)
     }
   } else {
     return state
