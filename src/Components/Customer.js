b7183d1b4f7caf2533948059696f374c9e9996d3
Laura Reale <laaru1988@gmail.com>
2020-09-27 12:52:07 +0200

    add alert on customer delete action

--- src/Components/Customer.js
+++ src/Components/Customer.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import ContractsList from './ContractsList';
 
+
 const Customer = ({ contracts, id, name, onDelete }) => {
+  const deleteCustomerAlert = (id) => {
+    if (window.confirm('This action will DELETE this customer PERMANENTLY. Are you sure you want to proceed?')) {
+      onDelete(id)
+      alert('Customer deleted successfully.')  
+    } else {
+      return alert('Action cancelled by the user. No changes were made.')
+    }
+
+  }
   return (
     <>
       {/* <img alt='customer' src='https://robohash.org/test' /> */}
       <h3>{name}</h3>
-      <button onClick={() => onDelete(id)}>Delete</button>
+      <button onClick={() => deleteCustomerAlert(id)}>Delete</button>
       <ContractsList customerContracts={contracts.filter((contract) => contract.customerId === id)} />
     </>
   )
