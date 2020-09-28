44975f8208393f83d092d8bb395db55f8ab82a67
Laura Reale <laaru1988@gmail.com>
2020-09-28 17:50:55 +0200

    fix Cypress tests

--- cypress/integration/UserCanNavigateTheAppAndPerformActions.js
+++ cypress/integration/UserCanNavigateTheAppAndPerformActions.js
@@ -5,7 +5,7 @@ beforeEach(() => {
 
 describe('User can visit the contracts list page', () => {
   it('and see a list of contracts', () => {
-    cy.get(':nth-child(1) > a').should('be.visible').click()
+    cy.get('[href="/contracts"]').should('be.visible').click()
     cy.get('#root').children().contains('Contracts:').should('not.contain', 'Customers:')
     cy.get('#root').children().contains('Nights watch hiring contract').should('not.contain', 'Jon Snow')
   })
@@ -13,19 +13,19 @@ describe('User can visit the contracts list page', () => {
 
 describe('User can visit the customers list page', () => {
   it('see a list of customers and their contracts', () => {
-    cy.get(':nth-child(2) > a').should('be.visible').click()
+    cy.get('[href="/customers"]').should('be.visible').click()
     cy.get('#root').children().contains('Customers:').should('not.contain', 'Contracts:')
   })
 
   it('and delete a chosen customer and relative contracts successfully', () => {
-    cy.get(':nth-child(2) > a').should('be.visible').click()
+    cy.get('[href="/customers"]').should('be.visible').click()
     cy.get('#root').children().should('contain', 'Jon Snow')
-    cy.get('#root > :nth-child(3)').click()
+    cy.get('#root > :nth-child(5)').click()
     cy.get('#root').children().should('not.contain', 'Jon Snow')
     cy.contains('Warriors hiring contract').should('not.contain', 'Nights watch hiring contract')
     cy.contains('Dragons regulatory contract').should('not.contain', 'Wolfs regulatory contract')
     cy.go('back')
-    cy.get(':nth-child(1) > a').should('be.visible').click()
+    cy.get('[href="/contracts"]').should('be.visible').click()
     cy.get('#root').contains('Contracts:').should('not.contain', 'Customers:')
     cy.get('h4').contains('Warriors hiring contract').should('not.contain', 'Nights watch hiring contract')
   })
